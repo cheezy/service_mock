aa2548fd84c8659619d26d06e5236094bb825f55
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2016-05-26 16:27:24 -0500

    added integration test for stub

--- spec/integration/integration_spec.rb
+++ spec/integration/integration_spec.rb
@@ -1,9 +1,11 @@
 require 'spec_helper'
+require 'net/http'
 
 describe 'Integration test' do
 
   #let(:mock) {ServiceMock::Server.new('1.57-standalone')}
   let(:mock) {ServiceMock::Server.new('standalone-2.0.10-beta')}
+  let(:http) {Net::HTTP.new('localhost', '8080')}
 
   it 'starts the server' do
     mock.start
@@ -18,6 +20,32 @@ describe 'Integration test' do
     mock.stop
     sleep 1
     expect(mock.process.exited?).to be true
+  end
+
+  it 'stubs a message' do
+    mock.start
+    sleep 2
+    message =
+    '
+    { "request":
+      {
+        "url": "/get/that",
+        "method": "GET"
+      },
+      "response":
+      {
+        "status": 200,
+        "body": "There it is\n"
+      }
+    }
+    '
+    mock.stub(message)
+    sleep 1
+    uri = URI('http://localhost:8080/get/that')
+    result =  Net::HTTP.get(uri)
+    expect(result).to eql "There it is\n"
+
+    mock.stop
   end
 end
 
