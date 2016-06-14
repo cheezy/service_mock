32db8b7f8c64e72e75bdb210ae249cd759fdf34d
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2016-06-14 13:37:10 -0500

    Added many of the outstanding command line options

--- lib/service_mock/command_line_options.rb
+++ lib/service_mock/command_line_options.rb
@@ -1,12 +1,13 @@
 module ServiceMock
   module CommandLineOptions
 
-    OPTIONS = [:port, :https_port, :verbose, :root_dir, :record_mappings, :proxy_all]
-    NOT_IMPLEMENTED = [:https_keystore, :keystore_password, :truststore_password,
-                       :https_require_client_cert, :preserve_host_header, :proxy_via,
-                       :enable_browser_proxying, :no_request_journal, :container_threads,
-                       :match_headers, :max_request_journal_entries, :jetty_acceptor_threads,
-                       :jetty_accept_queue_size,:jetty_header_buffer_size, :extensions, :help]
+    OPTIONS = [:port, :https_port, :verbose, :root_dir, :record_mappings, :proxy_all,
+               :match_headers, :proxy_via, :https_keystore, :keystore_password,
+               :enable_browser_proxying, :preserve_host_header, :https_truststore,
+               :truststore_password, :https_require_client_cert, :no_request_journal,
+               :max_request_journal_entries]
+    NOT_IMPLEMENTED = [:container_threads, :jetty_acceptor_threads, :jetty_accept_queue_size,
+                       :jetty_header_buffer_size, :extensions, :help]
 
     attr_accessor *OPTIONS
 
@@ -39,6 +40,50 @@ module ServiceMock
 
     def proxy_all_command
       ["--proxy-all=#{proxy_all.to_s}"]
+    end
+
+    def match_headers_command
+      ["--match-headers=\"#{match_headers}\""]
+    end
+
+    def proxy_via_command
+      ["--proxy-via #{proxy_via}"]
+    end
+
+    def https_keystore_command
+      ["--https-keystore #{https_keystore}"]
+    end
+
+    def keystore_password_command
+      ["--keystore-password #{keystore_password}"]
+    end
+
+    def enable_browser_proxying_command
+      ['--enable-browser-proxying']
+    end
+
+    def preserve_host_header_command
+      ['--preserve-host-header']
+    end
+
+    def https_truststore_command
+      ["--https-truststore #{https_truststore}"]
+    end
+
+    def truststore_password_command
+      ["--truststore-password #{truststore_password}"]
+    end
+
+    def https_require_client_cert_command
+      ['--https-require-client-cert']
+    end
+
+    def no_request_journal_command
+      ['--no-request-journal']
+    end
+
+    def max_request_journal_entries_command
+      ["--max-request-journal-entries #{max_request_journal_entries}"]
     end
   end
 end
--- spec/lib/command_line_options_spec.rb
+++ spec/lib/command_line_options_spec.rb
@@ -33,4 +33,59 @@ describe ServiceMock::CommandLineOptions do
     clo.proxy_all = 'http://proxy.ca'
     expect(clo.proxy_all_command).to eql ['--proxy-all=http://proxy.ca']
   end
+
+  it 'provides a match_headers option' do
+    clo.match_headers =  'Accept,Content-Type'
+    expect(clo.match_headers_command).to eql ['--match-headers="Accept,Content-Type"']
+  end
+
+  it 'provides a proxy_via option' do
+    clo.proxy_via = 'http://blah.com'
+    expect(clo.proxy_via_command).to eql ['--proxy-via http://blah.com']
+  end
+
+  it 'provides a https_keystore option' do
+    clo.https_keystore = '/path/to/keystore'
+    expect(clo.https_keystore_command).to eql ['--https-keystore /path/to/keystore']
+  end
+
+  it 'provides a keystore_password option' do
+    clo.keystore_password = 'passWord'
+    expect(clo.keystore_password_command).to eql ['--keystore-password passWord']
+  end
+
+  it 'provides a enable_borwser_proxying option' do
+    clo.enable_browser_proxying = true
+    expect(clo.enable_browser_proxying_command).to eql ['--enable-browser-proxying']
+  end
+
+  it 'provides a preserve_host_header option' do
+    clo.preserve_host_header = true
+    expect(clo.preserve_host_header_command).to eql ['--preserve-host-header']
+  end
+
+  it 'provides a https_truststore option' do
+    clo.https_truststore = '/path/to/truststore'
+    expect(clo.https_truststore_command).to eql ['--https-truststore /path/to/truststore']
+  end
+
+  it 'provides a truststore_password option' do
+    clo.truststore_password = 'secret'
+    expect(clo.truststore_password_command).to eql ['--truststore-password secret']
+  end
+
+  it 'provides a https_require_client_cert option' do
+    clo.https_require_client_cert = true
+    expect(clo.https_require_client_cert_command).to eql ['--https-require-client-cert']
+  end
+
+  it 'provides a no_request_journal option' do
+    clo.no_request_journal = true
+    expect(clo.no_request_journal_command).to eql ['--no-request-journal']
+  end
+
+  it 'provides a max_request_journal_entries option' do
+    clo.max_request_journal_entries = 40
+    expect(clo.max_request_journal_entries_command).to eql ['--max-request-journal-entries 40']
+  end
 end
