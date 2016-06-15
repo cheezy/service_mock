f85ddb19c2a5b19bf64baad6cf520bb1800fd971
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2016-06-15 09:25:46 -0500

    refactored to eliminate unnecessary local

--- lib/service_mock/server.rb
+++ lib/service_mock/server.rb
@@ -66,8 +66,7 @@ module ServiceMock
 
     def data_binding(hsh)
       OpenStruct.include ::ServiceMock::RenderSubTemplate
-      data_binding = OpenStruct.new(hsh).instance_eval { binding }
-      data_binding
+      OpenStruct.new(hsh).instance_eval { binding }
     end
 
     def start_process
